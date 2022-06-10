f5877e1456d0e4de6f0247b3e7351663d487022b
Milos <milosandric96@gmail.com>
2022-06-10 20:14:18 +0200

    Prvi commit

---
+++ WebAPI/WebAPI/Models/Faktura.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class Faktura
+    {
+        public int PIB { get; set; }
+        public int PIB2 { get; set; }
+        public string datumGenerisanja { get; set; }
+        public string datumPlacanja { get; set; }
+        public double ukupnaCena { get; set; }
+        public string tipFakture { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Preduzece.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class Preduzece
+    {
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string email { get; set; }
+        public string naziv { get; set; }
+        public string adresa { get; set; }
+        public double PIB { get; set; }
+    }
+}
