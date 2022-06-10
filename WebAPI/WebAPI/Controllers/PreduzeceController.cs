15de74d63cbc165c2a05e5f8c297d10c5275b6b3
Milos <milosandric96@gmail.com>
2022-06-10 20:48:18 +0200

    Drugi commit

---
+++ WebAPI/WebAPI/Controllers/PreduzeceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mojePreduzece.Models;
+
+namespace mojePreduzece.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreduzeceController : ControllerBase
+    {
+        Preduzece[] preduzece = new Preduzece[]
+        {
+            new Preduzece{ime="nesto",prezime="nesto2",email="nesto@gmail.com",naziv="nesto3",adresa="nesto4",PIB= 456234321}
+        };
+        [HttpGet]       
+        public IEnumerable<Preduzece> ukupanBrojPreduzeca()
+        {
+            return preduzece;
+        }
+
+    }
+}
--- WebAPI/WebAPI/Models/Preduzece.cs
+++ WebAPI/WebAPI/Models/Preduzece.cs
@@ -1,4 +1,4 @@
-﻿namespace WebAPI.Models
+﻿namespace mojePreduzece.Models
 {
     public class Preduzece
     {
