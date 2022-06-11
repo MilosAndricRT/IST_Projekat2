bc9f74c88a1dce3f31a9232d48e31ee7b102723b
Milos <milosandric96@gmail.com>
2022-06-11 21:17:05 +0200

    Cetvrti commit

--- Client/app.js
+++ Client/app.js
@@ -1,17 +1,50 @@
 let ucitavanje=document.getElementById("celina");
-let url="http://localhost:5292/api/Preduzece";
+let url="http://localhost:5292/api/Preduzece/ukupanBrojPreduzeca";
 let btn=document.getElementById("dugme");
 btn.addEventListener("click",()=>{
     fetch(url).then(resp=>resp.json()).then((data)=>{
+        ucitavanje.innerHTML="";
         data.forEach(element => {
-            const {ime,prezime,email,naziv,adresa,PIB}=element;
-            ucitavanje.innerHTML=`<ul><li>${ime}</li>
+            const {ime,prezime,email,naziv,adresa,pib}=element;
+            ucitavanje.innerHTML+=`<ul><li>${ime}</li>
             <li>${prezime}</li>
             <li>${email}</li>
             <li>${naziv}</li>
             <li>${adresa}</li>
-            <li>${PIB}</li></ul>`
+            <li>${pib}</li></ul>
+            <button onclick="izmeniPreduzece('${ime}','${prezime}','${email}','${naziv}','${adresa}','${pib}')">Izmeni</button>`
         });
     });
+})
+
+const izmeniPreduzece=(ime,prezime,email,naziv,adresa,pib)=>{
+    let url = "http://localhost:5292/api/Preduzece/izmeniPreduzece"
+    let root=document.getElementById("celina")
+    root.innerHTML=`<form action="${url}" method="post">
+    Ime: <input type="text" name="ime" value="${ime}"><br>
+    Prezime: <input type="text" name="prezime" value="${prezime}"><br>
+    Email: <input type="email" name="email" value="${email}"><br>
+    Naziv preduzeca: <input type="text" name="naziv" value="${naziv}"><br>
+    Adresa: <input type="text" name="adresa" value="${adresa}"><br>
+    PIB: <input type="number" name="PIB" value="${pib}"><br>
+    <input type="hidden" name="PIBstari" value="${pib}">
+    <button type="submit" name="">Izmeni</button>
+</form>`
+};
+const dodajPreduzece=()=>{
+    let url = "http://localhost:5292/api/Preduzece/dodajPreduzece"
+    let root=document.getElementById("celina")
+    root.innerHTML=`<form action="${url}" method="post">
+    Ime: <input type="text" name="ime"><br>
+    Prezime: <input type="text" name="prezime"><br>
+    Email: <input type="email" name="email"><br>
+    Naziv preduzeca: <input type="text" name="naziv"><br>
+    Adresa: <input type="text" name="adresa"><br>
+    PIB: <input type="number" name="PIB"><br>
+    <button type="submit" name="">Dodaj</button>
+</form>`
+};
+document.getElementById("dodajPreduzece").addEventListener("click",()=>{
+    dodajPreduzece();
 })
 
--- WebAPI/WebAPI/Controllers/PreduzeceController.cs
+++ WebAPI/WebAPI/Controllers/PreduzeceController.cs
@@ -8,17 +8,83 @@ namespace mojePreduzece.Controllers
     [ApiController]
     public class PreduzeceController : ControllerBase
     {
-        Preduzece[] preduzece = new Preduzece[]
+        static List<Preduzece> preduzeca = new List<Preduzece>()
         {
             new Preduzece{ime="Milan",prezime="Milanovic",email="milanmilanovic@gmail.com",naziv="PanAuto",adresa="Pancevo",PIB= 456234321},
             new Preduzece{ime="Luka",prezime="Lukic",email="lukalukic@gmail.com",naziv="Lancast",adresa="Novi Sad",PIB=273234592},
             new Preduzece{ime="Ana",prezime="Nikolic",email="ananikolic@gmail.com",naziv="IDJ",adresa ="Nis",PIB =537852917}
         };
-        [HttpGet]       
-        public IEnumerable<Preduzece> ukupanBrojPreduzeca()
+        [HttpGet("ukupanBrojPreduzeca")]       
+        public IActionResult ukupanBrojPreduzeca()
         {
-            return preduzece;
+            return Ok(preduzeca.OrderBy(Preduzece => Preduzece.PIB).ThenBy(Preduzece => Preduzece.naziv));
         }
 
+        [HttpPost("dodajPreduzece")]
+        public IActionResult dodavanje([FromForm] string ime, [FromForm] string prezime, [FromForm] string email, [FromForm] string naziv, [FromForm] string adresa, [FromForm] double PIB)
+        {
+            if (proveraPib(PIB))
+            {
+                return Ok("PIB je u netacnom formatu!!!");
+            }
+            foreach (Preduzece item in preduzeca)
+            {
+                if (item.PIB == PIB)
+                {
+                    return Ok("Vec postoji dati PIB!!!");
+                }
+            }
+            Preduzece preduzece = new Preduzece();
+            preduzece.ime = ime;
+            preduzece.prezime = prezime;
+            preduzece.email = email;
+            preduzece.naziv = naziv;
+            preduzece.adresa = adresa;
+            preduzece.PIB = PIB;
+            preduzeca.Add(preduzece);
+            return Ok(preduzece);
+        }
+
+        [HttpPost("izmeniPreduzece")]
+        public IActionResult izmeniPreduzece([FromForm] string ime, [FromForm] string prezime, [FromForm] string email, [FromForm] string naziv, [FromForm] string adresa, [FromForm] double PIB,[FromForm] double PIBstari)
+        {
+            if (proveraPib(PIB))
+            {
+                return Ok("PIB je u netacnom formatu!!!");
+            }
+            int pozicija = -1;
+            for (int i = 0; i < preduzeca.Count; i++)
+            {
+                Preduzece temp = preduzeca[i];
+                if(temp.PIB == PIBstari)
+                {
+                    pozicija = i;
+                    break;
+                }
+            }
+            if (pozicija == -1)
+            {
+                return Ok("Nismo pronasli preduzece koje trazite");
+            }
+            Preduzece preduzece = new Preduzece();
+            preduzece.ime = ime;
+            preduzece.prezime = prezime;
+            preduzece.email = email;
+            preduzece.naziv = naziv;
+            preduzece.adresa = adresa;
+            preduzece.PIB = PIB;
+            preduzeca[pozicija] = preduzece;
+            return Ok(preduzece);
+        }
+
+        private bool proveraPib(double PIB)
+        {
+            if(PIB<99999999 && PIB>1000000000)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
