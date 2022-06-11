fc154478d581db3d1e8cbd604fed310b2fca022b
Milos <milosandric96@gmail.com>
2022-06-11 21:40:55 +0200

    Peti commit

---
+++ WebAPI/WebAPI/Controllers/FakturaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mojePreduzece.Models;
+
+namespace mojePreduzece.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakturaController : ControllerBase
+    {
+        static List<Faktura> fakture = new List<Faktura>()
+        {
+            new Faktura{ PIB=456234321,PIB2=537852917,datumGenerisanja="testdatum",datumPlacanja="testdatumplacanja",ukupnaCena=1000,tipFakture="ulazna"}
+        };
+        [HttpGet("{PIB}")]
+        public IActionResult ukupanBrojPreduzeca(double PIB)
+        {
+            return Ok("TODO");
+        }
+    }
+}
